eee37a538d62818c2fe2904eb7627bb123daa767
TUAN THI <huetuan1984@gmail.com>
2015-12-30 12:03:16 -0500

    Some changes in this branch

--- ios-med-gitxcode/ViewController.swift
+++ ios-med-gitxcode/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Some changes to commit
-        
+        // Some random comments
     }
 
     override func didReceiveMemoryWarning() {
